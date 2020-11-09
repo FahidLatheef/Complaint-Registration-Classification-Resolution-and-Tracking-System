9a61cd803a9abf0c23ef03a95d508257ae89e243
Fahid Latheef A <fahideng@gmail.com>
2020-11-10 04:28:02 +0530

    Add files via upload

---
+++ Flask/password.py
@@ -0,0 +1,17 @@
+# username and password for admins
+
+user_credentials = {"Mortgage": ["admin01", "admin01"],
+                    "Debt collection": ["admin02", "admin02"],
+                    "Credit reporting": ["admin03", "admin03"],
+                    "Credit card or prepaid card": ["admin04", "admin04"],
+                    "Bank account or service": ["admin05", "admin05"],
+                    "Credit reporting, credit repair services, or other personal consumer reports": ["admin06",
+                                                                                                     "admin06"],
+                    "Student loan": ["admin07", "admin07"],
+                    "Consumer Loan": ["admin08", "admin08"],
+                    "Checking or savings account": ["admin09", "admin09"],
+                    "Payday loan, title loan, or personal loan": ["admin10", "admin10"],
+                    "Money transfers": ["admin11", "admin11"],
+                    "Vehicle loan or lease": ["admin12", "admin12"],
+                    "Other financial service": ["admin13", "admin13"]
+                    }
